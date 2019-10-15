3589fd1149bc032d8570eba71260538aa301105c
6gales <andrey@xgaminggroup.com>
2019-10-15 23:48:34 +0700

    initial commit

---
+++ ProductShopWebApi/ProductShopService.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace ProductShopWebApi
+{
+	[ServiceContract(Namespace = "")]
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+	public class ProductShopService
+	{
+
+		private static ProductStorage _storage = new ProductStorage(); 
+
+		[WebInvoke(Method = "GET",
+		   RequestFormat = WebMessageFormat.Json,
+		   ResponseFormat = WebMessageFormat.Json,
+		   UriTemplate = "/shop")]
+		public string GetAllProducts() => JsonConvert.SerializeObject(_storage.Products);
+
+
+		[WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json,
+			UriTemplate = "/shop", ResponseFormat = WebMessageFormat.Json,
+			BodyStyle = WebMessageBodyStyle.Wrapped)]
+		public void AddProduct(string str)
+		{
+
+		}
+
+
+		[WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json,
+			UriTemplate = "/shop/order", ResponseFormat = WebMessageFormat.Json,
+			BodyStyle = WebMessageBodyStyle.Wrapped)]
+		public void MakeOrder(string str)
+		{
+	
+		}
+
+		[WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json,
+			UriTemplate = "/shop", ResponseFormat = WebMessageFormat.Json,
+			BodyStyle = WebMessageBodyStyle.Wrapped)]
+		public void DeleteProduct(string shopId)
+		{
+		}
+	}
+}
---
+++ ProductShopWebApi/ProductStorage.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ProductShopWebApi
+{
+	public class ProductStorage
+	{
+		public IEnumerable<Product> Products { get => _productSet; }
+
+		private HashSet<Product> _productSet;
+
+		public ProductStorage()
+		{
+			using (StreamReader r = new StreamReader("InitialProductData.json"))
+			{
+				string json = r.ReadToEnd();
+				_productSet = JsonConvert.DeserializeObject<HashSet<Product>>(json);
+			}
+		}
+
+		public bool Add(Product product)
+		{
+			if (_productSet.Contains(product))
+				return false;
+
+			_productSet.Add(product);
+			return true;
+		}
+
+		public bool Delete(Product product)
+		{
+			if (_productSet.Contains(product))
+				return false;
+
+			_productSet.Add(product);
+			return true;
+		}
+	}
+}
